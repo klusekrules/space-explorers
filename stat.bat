74bfce23b9e197eba3c92a58a6bfb6adfc27f2d3
danielwojdak@gmail.com <danielwojdak@gmail.com@7d2b72b5-99d5-17bd-2a6a-8f8c28692ed1>
2013-07-08 18:52:43 +0000

    Zmiana skryptu generowania statystyk

--- stat.bat
+++ stat.bat
@@ -2,5 +2,5 @@ echo Rozpoczeto generowanie numeru wersji
 SubWCRev.exe %1 ..\svn_version.h.tmpl %1svn_version.h
 echo Zakonczono generowanie numeru wersji
 echo Rozpoczeto zbieranie statystyk
-cloc --by-file-by-lang --windows --out="%1doc\ProjStat.txt" --exclude-dir=out,allegro,Debug,doc,obj,SFML,parser --exclude-ext=cppcheck,sln,opensdf,rc,sdf,txt,tmpl,vsproj,filters,user,suo ../
+cloc --by-file-by-lang --windows --out="%1doc\ProjStat.txt" --exclude-dir=out,allegro,Debug,doc,obj,SFML,parser --exclude-ext=.cppcheck,.sln,.opensdf,.rc,.sdf,.txt,.tmpl,.vsproj,.filters,.user,.suo ../
 echo Zakonczono zbieranie statystyk
