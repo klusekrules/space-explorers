9ed8547380465b49923c6450a1e8d29b82a0b3e9
danielwojdak@gmail.com <danielwojdak@gmail.com@7d2b72b5-99d5-17bd-2a6a-8f8c28692ed1>
2013-07-11 07:15:33 +0000

    [Issue-54] [Issue-10] Refaktoryzacja i dokumentacja klasy ZmianaPotegowa

--- trunk/FuncTransf/ZmianaPotegowa.cpp
+++ trunk/FuncTransf/ZmianaPotegowa.cpp
@@ -4,8 +4,9 @@
 #include "..\XmlBO.h"
 #include "..\definicjeWezlowXML.h"
 
-ZmianaPotegowa::ZmianaPotegowa( TiXmlElement * e ) throw(int)
-	: wspolczynnik(XmlBO::ZnajdzWezelJezeli<NOTHROW>(e,WEZEL_XML_PARAM,ATRYBUT_XML_IDENTYFIKATOR,"1")), wykladnik(XmlBO::ZnajdzWezelJezeli<NOTHROW>(e,WEZEL_XML_PARAM,ATRYBUT_XML_IDENTYFIKATOR,"2"))
+ZmianaPotegowa::ZmianaPotegowa( TiXmlElement * wezel ) throw(int)
+	: wspolczynnik_(XmlBO::ZnajdzWezelJezeli<NOTHROW>( wezel, WEZEL_XML_PARAM, ATRYBUT_XML_IDENTYFIKATOR, "1" )),
+	wykladnik_(XmlBO::ZnajdzWezelJezeli<NOTHROW>( wezel, WEZEL_XML_PARAM, ATRYBUT_XML_IDENTYFIKATOR, "2" ))
 {
 }
 
@@ -13,21 +14,25 @@ ZmianaPotegowa::~ZmianaPotegowa(void)
 {
 }
 
-long double ZmianaPotegowa::policzWartosc(long double d, int p, int) const{
-	return d * wspolczynnik.pobierzWspolczynnik() * pow(static_cast<long double>(p),wykladnik.pobierzWspolczynnik());
+ZmianaInterfejs* ZmianaPotegowa::TworzZmianaPotegowa( TiXmlElement* wezel ){
+	return new ZmianaPotegowa(wezel);
+}
+
+long double ZmianaPotegowa::policzWartosc(long double wartosc, int poziom, int identyfikatorPlanety) const{
+	return wartosc * wspolczynnik_.pobierzWspolczynnik() * pow( static_cast<long double>(poziom), wykladnik_.pobierzWspolczynnik() );
 }
 
 ZmianaPotegowa* ZmianaPotegowa::Kopia()const{
 	return new ZmianaPotegowa(*this);
 }
 
-bool ZmianaPotegowa::RejestrujZmianaPotegowa( ZmianaFabryka &ref ){
-	return ref.rejestracjaZmiany(idKlasy,ZmianaPotegowa::TworzZmianaPotegowa);
+bool ZmianaPotegowa::RejestrujZmianaPotegowa( ZmianaFabryka &fabryka ){
+	return fabryka.rejestracjaZmiany( identyfikator_, ZmianaPotegowa::TworzZmianaPotegowa );
 }
 
 string ZmianaPotegowa::toString()const{
 	Logger str(CLASSNAME(ZmianaPotegowa));
-	str.addField("wspolczynnik",wspolczynnik);
-	str.addField("wykladnik",wykladnik);
+	str.addField( "wspolczynnik", wspolczynnik_ );
+	str.addField( "wykladnik", wykladnik_ );
 	return str.toString();
 }
