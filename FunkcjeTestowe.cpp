a9150b80c4b56873b7808138c0251232c8916770
danielwojdak@gmail.com <danielwojdak@gmail.com@7d2b72b5-99d5-17bd-2a6a-8f8c28692ed1>
2013-07-16 13:49:35 +0000

    Dodanie testów klasy Statek

--- FunkcjeTestowe.cpp
+++ FunkcjeTestowe.cpp
@@ -2,63 +2,6 @@
 #include "Testy.h"
 
 /*
-bool test_KlasaJednostkaAtakujaca( Test & t ){
-	shared_ptr<Statek> a( tworzStatek(t,Identyfikator(11),Ilosc(8)) );
-	Obrazenia oAtak = a->Atak();
-	Obrazenia baseAtak(a->getStatekInfo().getAtak(PodstawoweParametry(Poziom(1),Identyfikator()))()*8.0);
-	Obrazenia tbAtak(baseAtak() *(JednostkaAtakujaca::srednia-(JednostkaAtakujaca::odchylenie * 3 )));
-	Obrazenia teAtak(baseAtak() *(JednostkaAtakujaca::srednia+(JednostkaAtakujaca::odchylenie * 3 )));
-	Aplikacja::getInstance().getLog().debug(oAtak);
-	Aplikacja::getInstance().getLog().debug(baseAtak);
-	Aplikacja::getInstance().getLog().debug(tbAtak);
-	Aplikacja::getInstance().getLog().debug(teAtak);
-	t.assert_false(EXCEPTION_PLACE, tbAtak <= oAtak && oAtak <= teAtak);
-
-	Obrazenia oOslona = a->Oslona(oAtak);
-	Obrazenia baseOslona(a->getStatekInfo().getOslona(PodstawoweParametry(Poziom(1),Identyfikator()))()*8.0);
-	double tbd=oAtak() - baseOslona() *(JednostkaAtakujaca::srednia+(JednostkaAtakujaca::odchylenie * 3 ));
-	double ted=oAtak() - baseOslona() *(JednostkaAtakujaca::srednia-(JednostkaAtakujaca::odchylenie * 3 ));
-	Obrazenia tbOslona(tbd>0?tbd:0);
-	Obrazenia teOslona(ted>0?ted:0);
-	Aplikacja::getInstance().getLog().debug(oOslona);
-	Aplikacja::getInstance().getLog().debug(baseOslona);
-	Aplikacja::getInstance().getLog().debug(tbOslona);
-	Aplikacja::getInstance().getLog().debug(teOslona);
-	t.assert_false(EXCEPTION_PLACE, tbOslona <= oOslona && oOslona <= teOslona);
-
-	Obrazenia oPancerz = a->Pancerz(oOslona);
-	Obrazenia basePancerz(a->getStatekInfo().getPancerz(PodstawoweParametry(Poziom(1),Identyfikator()))()*8.0);
-	Obrazenia tbPancerz(basePancerz<oOslona?oOslona-basePancerz:Obrazenia(0));
-	Aplikacja::getInstance().getLog().debug(oPancerz);
-	Aplikacja::getInstance().getLog().debug(basePancerz);
-	Aplikacja::getInstance().getLog().debug(tbPancerz);
-	t.assert_false(EXCEPTION_PLACE, tbPancerz == oPancerz);
-	return true;
-}
-
-bool test_Issue42( Test & t ){
-	shared_ptr<Statek> a( tworzStatek(t,Identyfikator(12),Ilosc(2)) );
-	t.assert_false(EXCEPTION_PLACE, a->getMasa() == Masa(4+14));
-	t.assert_false(EXCEPTION_PLACE, a->getObjetosc() == Objetosc(6));
-	t.assert_false(EXCEPTION_PLACE, a->getPowierzchnia() == Powierzchnia(8));
-	t.assert_false(EXCEPTION_PLACE, a->getPojemnoscMax() == Objetosc(1300*12*2));
-	t.assert_false(EXCEPTION_PLACE, a->getJednostkoweZuzyciePaliwa() == ZuzyciePaliwa(12));
-	t.assert_false(EXCEPTION_PLACE, a->getMocSilnika() == MocSilnika(5));
-	t.assert_false(EXCEPTION_PLACE, a->getSprawnoscSilnika() == Fluktuacja(0.97f*8.0f));
-	t.assert_false(EXCEPTION_PLACE, a->getAtak() == Obrazenia(9.0*90*2));
-	t.assert_false(EXCEPTION_PLACE, a->getPancerz() == Obrazenia(10.0*250*2));
-	t.assert_false(EXCEPTION_PLACE, a->getOslona() == Obrazenia(11.0*300*2));
-	return true;
-}
-
-bool test_Issue52( Test & t ){
-	shared_ptr<Statek> a( tworzStatek(t,Identyfikator(13),Ilosc(1)) );
-	t.assert_false(EXCEPTION_PLACE, a->getPoziom() == Poziom(4));
-	shared_ptr<Statek> b( tworzStatek(t,Identyfikator(14),Ilosc(1)) );
-	t.assert_false(EXCEPTION_PLACE, b->getPoziom() == Poziom(2));
-	return true;
-}
-
 bool test_Technologie( Test & t ){
 	shared_ptr<Technologia> a( tworzTechnologie(t,Identyfikator(15)) );
 	Aplikacja::getInstance().getLog().debug(*a);
@@ -97,25 +40,6 @@ bool test_Budynki( Test & t ){
 	t.assert_false(EXCEPTION_PLACE, p.size()==0);
 	z = c->PobierzZapotrzebowanie();
 	t.assert_false(EXCEPTION_PLACE, z.size()==0);
-	return true;
-}
-
-bool test_KlasyFlota( Test & t ){
-	auto idPlanety = Aplikacja::getInstance().getGra().generujPlanete();
-	t.assert_false(EXCEPTION_PLACE,Aplikacja::getInstance().getGra().przeniesPlaneteDoUzytkownika(idPlanety));
-	Planeta& planeta = Aplikacja::getInstance().getGra().getUzytkownik().getPlaneta(idPlanety);
-	t.assert_false(EXCEPTION_PLACE,planeta.wybuduj(Identyfikator(0x1),Ilosc(20000)));
-	t.assert_false(EXCEPTION_PLACE,planeta.wybuduj(Identyfikator(0x4),Ilosc(20000)));
-	t.assert_false(EXCEPTION_PLACE,planeta.wybuduj(Identyfikator(0x7),Ilosc(20000)));
-	t.assert_false(EXCEPTION_PLACE,planeta.wybuduj(Identyfikator(0xB),Ilosc(1000)));
-	t.assert_false(EXCEPTION_PLACE,planeta.wybuduj(Identyfikator(0xE),Ilosc(2000)));
-	auto idFloty = planeta.dodajFlote();
-	t.assert_false(EXCEPTION_PLACE,planeta.przeniesDoFloty(idFloty,Identyfikator(0xB),Ilosc(500)));
-	t.assert_false(EXCEPTION_PLACE,planeta.przeniesDoFloty(idFloty,Identyfikator(0xE),Ilosc(1000)));
-	
-	t.assert_false(EXCEPTION_PLACE,planeta.zaladujFlote(idFloty,Identyfikator(0x1),Ilosc(500)));
-	t.assert_false(EXCEPTION_PLACE,planeta.zaladujFlote(idFloty,Identyfikator(0xE),Ilosc(900)));
-
 	return true;
 }
 */
---
+++ StatekTest.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "TestyJednostkowe.h"
+#include "Aplikacja.h"
+
+class StatekTest: 
+	public PaczkaTestow
+{
+	TWORZ_PACZKE_TESTOW(StatekTest);
+		DODAJ_TEST(Issue42Test);
+		DODAJ_TEST(Issue52Test);
+		DODAJ_TEST(JednostkaAtakujacaTest);
+		DODAJ_TEST(FlotaTest);
+	TWORZ_PACZKE_TESTOW_END();
+public:
+	void startTestow() override;
+	void Issue42Test();
+	void Issue52Test();
+	void JednostkaAtakujacaTest();
+	void FlotaTest();
+	StatekTest(void);
+	virtual ~StatekTest(void);
+private:
+	const Statek* a;
+	const Statek* b;
+	const Statek* c;
+	const Statek* d;
+	Planeta* planeta;
+};
+
