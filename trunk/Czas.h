09190a51faabdd9659fc4a483596ace746f43f7f
danielwojdak@gmail.com <danielwojdak@gmail.com@7d2b72b5-99d5-17bd-2a6a-8f8c28692ed1>
2013-07-18 10:13:49 +0000

    [Issue-54] [Issue-10] Refaktoryzacja i dokumentacja klas: Bool, Czas, Dystans, Fluktuacja.

--- trunk/Czas.h
+++ trunk/Czas.h
@@ -1,25 +1,77 @@
 #pragma once
-#include "Logger\LoggerInterface.h" 
-#include "Main.h"
+#include "Logger\LoggerInterface.h"
 #include "TypyProste.h"
-
+/**
+* \brief Klas przechowujaca ilosc czasu.
+*
+* Klasa reprezentuj¹ca ilosc czasu.
+* \author Daniel Wojdak
+* \version 1
+* \date 18-07-2013
+*/
 class Czas:
 	public LoggerInterface,
 	public PodstawowyInterfejs< SPG::Czas >
 {
 public:	
-	Czas();
-	Czas( const Czas& );
-	explicit Czas( const type_name& );
-	virtual ~Czas();
-	
-	bool operator==( const Czas& )const;
-	bool operator!=( const Czas& )const;
 
-	Czas& operator=( const Czas& );
-	Czas& operator+=( const Czas& );
-	Czas& operator=( const type_name& b );
-	
+	/**
+	* \brief Konstruktor.
+	*/
+	Czas();
+
+	/**
+	* \brief Konstruktor kopiuj¹cy.
+	* Metoda ustawia wartoœæ atrybutu na tak¹ jak jest podana w parametrze konstruktora.
+	* \param[in] wartosc - Wartoœæ na jak¹ ma byæ ustawiony atrybut klasy.
+	*/
+	Czas( const Czas& wartosc );
+
+	/**
+	* \brief Konstruktor.
+	* Metoda ustawia wartoœæ atrybutu na tak¹ jak jest podana w parametrze konstruktora.
+	* \param[in] wartosc - Wartoœæ na jak¹ ma byæ ustawiony atrybut klasy.
+	*/
+	explicit Czas( const type_name& wartosc );
+
+	/**
+	* \brief Destruktor.
+	*/
+	virtual ~Czas();
+
+	/**
+	* \brief Operator równoœci.
+	* \param[in] wartosc - Wartoœæ porównywana z atrybutem klasy.
+	*/
+	bool operator==( const Czas& wartosc )const;
+
+	/**
+	* \brief Operator nierownoœci.
+	* \param[in] wartosc - Wartoœæ porównywana z atrybutem klasy.
+	*/
+	bool operator!=( const Czas& wartosc )const;
+
+	/**
+	* \brief Operator przypisania.
+	* \param[in] wartosc - Wartoœæ na jak¹ ma byæ ustawiony atrybut klasy.
+	*/
+	Czas& operator=( const Czas& wartosc );
+
+	/**
+	* \brief Operator przypisania i sumy.
+	* \param[in] wartosc - Wartoœæ na jak¹ ma byæ dodana do atrybut klasy.
+	*/
+	Czas& operator+=( const Czas& wartosc );
+
+	/**
+	* \brief Operator przypisania.
+	* \param[in] wartosc - Wartoœæ na jak¹ ma byæ ustawiony atrybut klasy.
+	*/
+	Czas& operator=( const type_name& wartosc );
+
+	/**
+	* Funkcja s³u¿¹ca jako podstawa do tworzenia napisów z opisem klasy.
+	* \return Napis zawieraj¹cy opis klasy.
+	*/
 	string napis() const override;		 
 };
-
