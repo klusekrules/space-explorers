65243840eae33ef5a76d5f2feb8e90396f479fc8
danielwojdak@gmail.com <danielwojdak@gmail.com@7d2b72b5-99d5-17bd-2a6a-8f8c28692ed1>
2013-07-23 13:22:05 +0000

    [Issue-54] [Issue-10] Refaktoryzacja i dokumentacja klas: Wymagania, LiczenieKosztów, LiczenieWymogow, SzablonKosztow. Poprawa metod w ObiektBazowy. Poprawa tworzenia napisu w klasie BudynekInfo.

--- trunk/SzablonKosztow.h
+++ trunk/SzablonKosztow.h
@@ -1,18 +1,31 @@
 #pragma once
-#include "Logger\LoggerInterface.h"
 #include "FuncTransf\ZmianaInterfejs.h"
 #include "parser\ticpp.h"
 #include "definicjeWezlowXML.h"
 #include "Utils.h"
 #include <functional>
-
+/**
+* \brief Szablon do tworzenia klasy kosztów lub wymogów.
+* 
+* Zarz¹dza kosztem lub wymogiem.
+* \author Daniel Wojdak
+* \version 1
+* \date 23-07-2013
+*/
 template< class T >
-class SzablonKosztow :
-	virtual public LoggerInterface
+class SzablonKosztow
 {
 public:
 	typedef shared_ptr<T> TypObiektu;
 	typedef shared_ptr<ZmianaInterfejs> Zmiana;
+
+	/**
+	* \brief Konstruktor.
+	*
+	* Konstruktor tworz¹cy obiekt na podstawie wêz³a xml.
+	* \param[in] wezel - Wêze³ na podstawie, którego jest tworzony obiekt.
+	* \param[in] nazwa - Nazwa wêz³a obiektu.
+	*/
 	SzablonKosztow( TiXmlElement* wezel , string nazwa ) 
 		: obiekt_(nullptr), zmiana_(nullptr)
 	{
@@ -25,19 +38,36 @@ public:
 		}
 	}
 
-	virtual ~SzablonKosztow(void)
-	{
-	}
-
+	/**
+	* \brief Metoda wykonuj¹ca akcje na obiekcie.
+	*
+	* Metoda wykonuje dowoln¹ akcjê na obiekcie.
+	* \param[in,out] funkcja - Funktor wykonuj¹cy akcje na obiekcie.
+	* \return Wartoœæ zwracana przez funktor.
+	*/
 	bool wykonaj( function<bool(TypObiektu,Zmiana)> funkcja ){
 		return funkcja(obiekt_,zmiana_);
 	}
 
-	string napis()const{
-		return string();
+	/**
+	* \brief Metoda pobieraj¹ca atrybut.
+	*
+	* \return WskaŸnik na przechowywany obiekt.
+	*/
+	TypObiektu pobierzObiekt() const{
+		return obiekt_;
 	}
-private:
-	Zmiana zmiana_;
-	TypObiektu obiekt_;
-};
 
+	/**
+	* \brief Metoda pobieraj¹ca atrybut.
+	*
+	* \return WskaŸnik na przechowywan¹ zmianê.
+	*/
+	Zmiana pobierzZmiane()const{
+		return zmiana_;
+	}
+
+private:
+	Zmiana zmiana_; /// WskaŸnika na zmianê parametru obiektu.
+	TypObiektu obiekt_; /// WskaŸnik na obiekt.
+};
