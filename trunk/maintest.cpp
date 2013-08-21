344b0dcfc177b02a334b16253845a7c7d3f7bf5c
danielwojdak@gmail.com <danielwojdak@gmail.com@7d2b72b5-99d5-17bd-2a6a-8f8c28692ed1>
2013-08-21 10:01:28 +0000

    Zmiana funkcji main

--- trunk/maintest.cpp
+++ trunk/maintest.cpp
@@ -5,7 +5,7 @@
 #ifdef TESTS
 #include "TestyJednostkowe.h"
 
-void main(){
+void main( int argv , char* argc[] ){
 	_CrtSetDbgFlag ( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
 	_CrtSetReportMode( _CRT_WARN, _CRTDBG_MODE_FILE );
     _CrtSetReportFile( _CRT_WARN, _CRTDBG_FILE_STDOUT );
