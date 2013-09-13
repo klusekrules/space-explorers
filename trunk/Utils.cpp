bc6fcd61a5f797f76a1b59449675f0b2cef56fa3
danielwojdak@gmail.com <danielwojdak@gmail.com@7d2b72b5-99d5-17bd-2a6a-8f8c28692ed1>
2013-09-13 13:22:31 +0000

    [Issue-58] Dodanie metody tworzącej sumę kontrolną sha-3 512 bitową.

--- trunk/Utils.cpp
+++ trunk/Utils.cpp
@@ -1,6 +1,7 @@
 #include "Utils.h"
 #include "Aplikacja.h"
 #include "WyjatekParseraXML.h"
+#include "keccak.h"
 
 shared_ptr<ZmianaInterfejs> Utils::TworzZmiane( TiXmlElement* wezel ){
 	if(!wezel)
@@ -16,4 +17,32 @@ void Utils::generujWyjatekBleduStruktury( const Tekst& plik, const Ilosc& linia,
 	std::string komunikat("B³¹d struktry pliku xml. Niepoprawny wêze³: ");
 	komunikat << *wezel;
 	throw WyjatekParseraXML(plik,linia,exception(komunikat.c_str()),Tekst("B³¹d struktry pliku xml."));
+}
+
+void Utils::ascii2hex(string& str, unsigned char c){
+	char pierwszy = (c >> 4) & 0x0F, drugi = c & 0x0F;
+	if(pierwszy < 10){
+		pierwszy += 0x30;
+	}else{
+		pierwszy += 55;
+	}
+
+	if(drugi < 10){
+		drugi += 0x30;
+	}else{
+		drugi += 55;
+	}
+	str.push_back(pierwszy);
+	str.push_back(drugi);
+}
+
+void Utils::sha3(string& str){
+	uint8_t md[64];
+	memset(md,0,64);
+	keccak(reinterpret_cast<const uint8_t* >(str.c_str()),static_cast<int>(str.size()),md,64);
+	str.clear();
+	str.reserve(128);
+	for(int a=0; a<64;++a){
+		Utils::ascii2hex(str,md[a]);
+	}
 }
