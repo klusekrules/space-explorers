66406ca8d58fbbb854deee1ba2ab287cc06830da
danielwojdak@gmail.com <danielwojdak@gmail.com@7d2b72b5-99d5-17bd-2a6a-8f8c28692ed1>
2013-10-18 11:46:47 +0000

    [Issue-74] Poprawa wysypujacego się testu w release

--- ZleceniaTest.cpp
+++ ZleceniaTest.cpp
@@ -17,88 +17,97 @@ void ZleceniaTest::startTestow(){
 
 void ZleceniaTest::tranzakcja_poprawna(){
 	MenedzerTranzakcji tranzakcja;
-	shared_ptr<Statek> statek(gra->pobierzStatek(Identyfikator(0xD)).tworzEgzemplarz(Ilosc(1),Identyfikator(0x0)));
-
-	tranzakcja.dodaj( make_shared< Zlecenie< Ilosc , shared_ptr<Statek> > >( Ilosc(2) , statek
+	Ilosc jeden(1);
+	shared_ptr<Statek> statek(gra->pobierzStatek(Identyfikator(0xD)).tworzEgzemplarz(jeden,Identyfikator(0x0)));
+	Ilosc dwa(2);
+	Ilosc pietnascie(15);
+	tranzakcja.dodaj( make_shared< Zlecenie< Ilosc , shared_ptr<Statek> > >( dwa , statek
 		, [](Ilosc& i , shared_ptr<Statek>& s)->bool{s->ustawIlosc(s->pobierzIlosc()+i); return true;}
 		, [](Ilosc& i , shared_ptr<Statek>& s)->bool{s->ustawIlosc(s->pobierzIlosc()-i); return true;} 
 		) );
-	tranzakcja.dodaj( make_shared< Zlecenie< Ilosc , shared_ptr<Statek> > >( Ilosc(2) , statek
+	tranzakcja.dodaj( make_shared< Zlecenie< Ilosc , shared_ptr<Statek> > >( dwa , statek
 		, [](Ilosc& i , shared_ptr<Statek>& s)->bool{s->ustawIlosc(s->pobierzIlosc()*i); return true;}
 		, [](Ilosc& i , shared_ptr<Statek>& s)->bool{s->ustawIlosc(s->pobierzIlosc()/i); return true;} 
 		) );
-	tranzakcja.dodaj( make_shared< Zlecenie< Ilosc , shared_ptr<Statek> > >( Ilosc(15) , statek
+	tranzakcja.dodaj( make_shared< Zlecenie< Ilosc , shared_ptr<Statek> > >( pietnascie , statek
 		, [](Ilosc& i , shared_ptr<Statek>& s)->bool{s->ustawIlosc(s->pobierzIlosc()+i); return true;}
 		, [](Ilosc& i , shared_ptr<Statek>& s)->bool{s->ustawIlosc(s->pobierzIlosc()-i); return true;} 
 		) );
 	UNIT_TEST_ASSERT_TRUE(tranzakcja.wykonaj());
-	UNIT_TEST_ASSERT_EQUAL(Ilosc(21), statek->pobierzIlosc());
+	Ilosc dwadziesciajeden(21);
+	UNIT_TEST_ASSERT_EQUAL(dwadziesciajeden, statek->pobierzIlosc());
 }
 
 void ZleceniaTest::tranzakcja_cofnieta(){
 	MenedzerTranzakcji tranzakcja;
-	shared_ptr<Statek> statek(gra->pobierzStatek(Identyfikator(0xD)).tworzEgzemplarz(Ilosc(1),Identyfikator(0x0)));
-
-	tranzakcja.dodaj( make_shared< Zlecenie< Ilosc , shared_ptr<Statek> > >( Ilosc(2) , statek
+	Ilosc jeden(1);
+	shared_ptr<Statek> statek(gra->pobierzStatek(Identyfikator(0xD)).tworzEgzemplarz(jeden,Identyfikator(0x0)));
+	Ilosc dwa(2);
+	Ilosc pietnascie(15);
+	tranzakcja.dodaj( make_shared< Zlecenie< Ilosc , shared_ptr<Statek> > >( dwa , statek
 		, [](Ilosc& i , shared_ptr<Statek>& s)->bool{s->ustawIlosc(s->pobierzIlosc()+i); return true;}
 		, [](Ilosc& i , shared_ptr<Statek>& s)->bool{s->ustawIlosc(s->pobierzIlosc()-i); return true;} 
 		) );
-	tranzakcja.dodaj( make_shared< Zlecenie< Ilosc , shared_ptr<Statek> > >( Ilosc(2) , statek
+	tranzakcja.dodaj( make_shared< Zlecenie< Ilosc , shared_ptr<Statek> > >( dwa , statek
 		, [](Ilosc& i , shared_ptr<Statek>& s)->bool{s->ustawIlosc(s->pobierzIlosc()*i); return true;}
 		, [](Ilosc& i , shared_ptr<Statek>& s)->bool{s->ustawIlosc(s->pobierzIlosc()/i); return true;} 
 		) );
-	tranzakcja.dodaj( make_shared< Zlecenie< Ilosc , shared_ptr<Statek> > >( Ilosc(15) , statek
+	tranzakcja.dodaj( make_shared< Zlecenie< Ilosc , shared_ptr<Statek> > >( pietnascie , statek
 		, [](Ilosc& i , shared_ptr<Statek>& s)->bool{s->ustawIlosc(s->pobierzIlosc()+i); return true;}
 		, [](Ilosc& i , shared_ptr<Statek>& s)->bool{s->ustawIlosc(s->pobierzIlosc()-i); return true;} 
 		) );
 	tranzakcja.dodaj( make_shared< Operacja >( ) );
 	UNIT_TEST_ASSERT_FALSE(tranzakcja.wykonaj());
-	UNIT_TEST_ASSERT_EQUAL(Ilosc(1), statek->pobierzIlosc());
+	UNIT_TEST_ASSERT_EQUAL(jeden, statek->pobierzIlosc());
 }
 
 void ZleceniaTest::tranzakcja_throw(){
 	MenedzerTranzakcji tranzakcja;
-	shared_ptr<Statek> statek(gra->pobierzStatek(Identyfikator(0xD)).tworzEgzemplarz(Ilosc(1),Identyfikator(0x0)));
-
-	tranzakcja.dodaj( make_shared< Zlecenie< Ilosc , shared_ptr<Statek> > >( Ilosc(2) , statek
+	Ilosc jeden(1);
+	shared_ptr<Statek> statek(gra->pobierzStatek(Identyfikator(0xD)).tworzEgzemplarz(jeden,Identyfikator(0x0)));
+	Ilosc dwa(2);
+	Ilosc pietnascie(15);
+	tranzakcja.dodaj( make_shared< Zlecenie< Ilosc , shared_ptr<Statek> > >( dwa , statek
 		, [](Ilosc& i , shared_ptr<Statek>& s)->bool{s->ustawIlosc(s->pobierzIlosc()+i); return true;}
 		, [](Ilosc& i , shared_ptr<Statek>& s)->bool{s->ustawIlosc(s->pobierzIlosc()-i); return true;} 
 		) );
-	tranzakcja.dodaj( make_shared< Zlecenie< Ilosc , shared_ptr<Statek> > >( Ilosc(2) , statek
+	tranzakcja.dodaj( make_shared< Zlecenie< Ilosc , shared_ptr<Statek> > >( dwa , statek
 		, [](Ilosc& i , shared_ptr<Statek>& s)->bool{s->ustawIlosc(s->pobierzIlosc()*i); return true;}
 		, [](Ilosc& i , shared_ptr<Statek>& s)->bool{s->ustawIlosc(s->pobierzIlosc()/i); return true;} 
 		) );
-	tranzakcja.dodaj( make_shared< Zlecenie< Ilosc , shared_ptr<Statek> > >( Ilosc(15) , statek
+	tranzakcja.dodaj( make_shared< Zlecenie< Ilosc , shared_ptr<Statek> > >( pietnascie , statek
 		, [](Ilosc& i , shared_ptr<Statek>& s)->bool{s->ustawIlosc(s->pobierzIlosc()+i); return true;}
 		, [](Ilosc& i , shared_ptr<Statek>& s)->bool{s->ustawIlosc(s->pobierzIlosc()-i); return true;} 
 		) );
-	tranzakcja.dodaj( make_shared< Zlecenie< Ilosc , shared_ptr<Statek> > >( Ilosc(15) , statek
+	tranzakcja.dodaj( make_shared< Zlecenie< Ilosc , shared_ptr<Statek> > >( pietnascie , statek
 		, [](Ilosc& i , shared_ptr<Statek>& s)->bool{throw OgolnyWyjatek(EXCEPTION_PLACE);}
 		, [](Ilosc& i , shared_ptr<Statek>& s)->bool{throw OgolnyWyjatek(EXCEPTION_PLACE);}
 		) );
 	Aplikacja::pobierzInstancje().pobierzLogger().zablokujLogiWarn();
 	UNIT_TEST_ASSERT_FALSE(tranzakcja.wykonaj());
 	Aplikacja::pobierzInstancje().pobierzLogger().odblokujLogiWarn();
-	UNIT_TEST_ASSERT_EQUAL(Ilosc(1), statek->pobierzIlosc());
+	UNIT_TEST_ASSERT_EQUAL(jeden, statek->pobierzIlosc());
 }
 
 void ZleceniaTest::tranzakcja_throw2(){
 	MenedzerTranzakcji tranzakcja;
-	shared_ptr<Statek> statek(gra->pobierzStatek(Identyfikator(0xD)).tworzEgzemplarz(Ilosc(1),Identyfikator(0x0)));
-
-	tranzakcja.dodaj( make_shared< Zlecenie< Ilosc , shared_ptr<Statek> > >( Ilosc(2) , statek
+	Ilosc jeden(1);
+	shared_ptr<Statek> statek(gra->pobierzStatek(Identyfikator(0xD)).tworzEgzemplarz(jeden,Identyfikator(0x0)));
+	Ilosc dwa(2);
+	Ilosc pietnascie(15);	
+	tranzakcja.dodaj( make_shared< Zlecenie< Ilosc , shared_ptr<Statek> > >( dwa , statek
 		, [](Ilosc& i , shared_ptr<Statek>& s)->bool{s->ustawIlosc(s->pobierzIlosc()+i); return true;}
 		, [](Ilosc& i , shared_ptr<Statek>& s)->bool{throw OgolnyWyjatek(EXCEPTION_PLACE);} 
 		) );
-	tranzakcja.dodaj( make_shared< Zlecenie< Ilosc , shared_ptr<Statek> > >( Ilosc(2) , statek
+	tranzakcja.dodaj( make_shared< Zlecenie< Ilosc , shared_ptr<Statek> > >( dwa , statek
 		, [](Ilosc& i , shared_ptr<Statek>& s)->bool{s->ustawIlosc(s->pobierzIlosc()*i); return true;}
 		, [](Ilosc& i , shared_ptr<Statek>& s)->bool{s->ustawIlosc(s->pobierzIlosc()/i); return true;} 
 		) );
-	tranzakcja.dodaj( make_shared< Zlecenie< Ilosc , shared_ptr<Statek> > >( Ilosc(15) , statek
+	tranzakcja.dodaj( make_shared< Zlecenie< Ilosc , shared_ptr<Statek> > >( pietnascie , statek
 		, [](Ilosc& i , shared_ptr<Statek>& s)->bool{s->ustawIlosc(s->pobierzIlosc()+i); return true;}
 		, [](Ilosc& i , shared_ptr<Statek>& s)->bool{s->ustawIlosc(s->pobierzIlosc()-i); return true;} 
 		) );
-	tranzakcja.dodaj( make_shared< Zlecenie< Ilosc , shared_ptr<Statek> > >( Ilosc(15) , statek
+	tranzakcja.dodaj( make_shared< Zlecenie< Ilosc , shared_ptr<Statek> > >( pietnascie , statek
 		, [](Ilosc& i , shared_ptr<Statek>& s)->bool{throw OgolnyWyjatek(EXCEPTION_PLACE);}
 		, [](Ilosc& i , shared_ptr<Statek>& s)->bool{throw OgolnyWyjatek(EXCEPTION_PLACE);}
 		) );
@@ -108,7 +117,8 @@ void ZleceniaTest::tranzakcja_throw2(){
 		Aplikacja::pobierzInstancje().pobierzLogger().odblokujLogiWarn();
 	}catch(OgolnyWyjatek&){
 		Aplikacja::pobierzInstancje().pobierzLogger().odblokujLogiWarn();
-		UNIT_TEST_ASSERT_EQUAL(Ilosc(3), statek->pobierzIlosc());
+		Ilosc trzy(3);
+		UNIT_TEST_ASSERT_EQUAL(trzy, statek->pobierzIlosc());
 		//Aplikacja::pobierzInstancje().pobierzLogger().error(e.generujKomunikat());
 		return;
 	}
