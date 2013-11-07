2d700b6a434df4b606071df79a3b9c8055aab9d1
danielwojdak@gmail.com <danielwojdak@gmail.com@7d2b72b5-99d5-17bd-2a6a-8f8c28692ed1>
2013-11-07 14:48:24 +0000

    Dodanie prostej biblioteki (TGUI) do tworzenia GUI pod SFML

--- trunk/stdafx.h
+++ trunk/stdafx.h
@@ -1,5 +1,23 @@
 #pragma once
 
+#pragma warning( disable : 4251 )
+#pragma warning( disable : 4275 )
+
+#ifdef _DEBUG
+#ifdef _WIN64
+#pragma comment( lib, "TGUI-x64-d" )
+#else if _WIN32
+#pragma comment( lib, "TGUI-x86-d" )
+#endif
+#else
+#ifdef _WIN64
+#pragma comment( lib, "TGUI-x64-r" )
+#else if _WIN32
+#pragma comment( lib, "TGUI-x86-r" )
+#endif
+#endif
+
+
 #include <map>
 #include <iostream>
 #include <cassert>
@@ -12,5 +30,6 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 #include <SFML/Audio.hpp>
+#include <TGUI/TGUI.hpp>
 #include "RoundedRectangleShape.hpp"
 
