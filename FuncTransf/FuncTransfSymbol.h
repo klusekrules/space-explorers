2b942c23c39e5e0e93fda42c407e6e183e404cd5
danielwojdak@gmail.com <danielwojdak@gmail.com@7d2b72b5-99d5-17bd-2a6a-8f8c28692ed1>
2014-01-20 10:56:27 +0000

    Usuniecie projektu FuncTransf

--- FuncTransf/FuncTransfSymbol.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#pragma warning( disable : 4251 )
-#pragma warning( disable : 4275 )
-#ifdef FUNCTRANSF_EXPORTS
-#define FUNCTRANSF_API __declspec(dllexport)
-#else
-#define FUNCTRANSF_API __declspec(dllimport)
-#endif
