81597405b416c0580ddd91c6503c49f5b7d026dc
danielwojdak@gmail.com <danielwojdak@gmail.com@7d2b72b5-99d5-17bd-2a6a-8f8c28692ed1>
2014-05-15 13:03:28 +0000

    Dodanie klasy błędu konfiguracji aplikacji. Opakowanie wyjątków aplikacji w tę klasę.

--- trunk/BladKonfiguracjiEkranu.cpp
+++ trunk/BladKonfiguracjiEkranu.cpp
@@ -3,23 +3,21 @@
 #include "Aplikacja.h"
 
 namespace SpEx{
-	const STyp::Identyfikator BladKonfiguracjiEkranu::idBladKonfiguracjiEkranu(0x7);
-
 	BladKonfiguracjiEkranu::BladKonfiguracjiEkranu(const STyp::Tekst& tPlik, const STyp::Tekst& funkcja, const STyp::Ilosc& iLinia, const STyp::Identyfikator& ekran, const STyp::Tekst& komunikat)
 		: Wyjatek(tPlik, funkcja, iLinia, Aplikacja::pobierzInstancje().pobierzSladStosu(), idBladKonfiguracjiEkranu, STyp::Tekst("B³¹d konfigurowania okna!")
 		, STyp::Tekst("Podczas konfigurowania okna o id: "+ ekran.napis() + " wyst¹pi³ wyj¹tek. Komunikat b³êdu : ") + komunikat),
-		identyfikator_(ekran)
+		identyfikatorEkranu_(ekran)
 	{
 	}
 	
 	const STyp::Identyfikator& BladKonfiguracjiEkranu::pobierzIdEkranu() const{
-		return identyfikator_;
+		return identyfikatorEkranu_;
 	}
 
 	std::string BladKonfiguracjiEkranu::napis() const{
 		SLog::Logger str(NAZWAKLASY(BladKonfiguracjiEkranu));
 		str.dodajKlase(Wyjatek::napis());
-		str.dodajPole(NAZWAPOLA(identyfikator_), identyfikator_);
+		str.dodajPole(NAZWAPOLA(identyfikatorEkranu_), identyfikatorEkranu_);
 		return str.napis();
 	}
 };
--- trunk/Wyjatki.cpp
+++ trunk/Wyjatki.cpp
@@ -2,22 +2,27 @@
 #include "NieznalezionoObiektu.h"
 #include "NiepoprawneParametryFunkcji.h"
 #include "NiepowodzenieTranzakcji.h"
+#include "BladKonfiguracjiEkranu.h"
+#include "BladKonfiguracjiAplikacji.h"
 
 namespace SpEx{
 
-	const STyp::Identyfikator	NiezgodnyTypSurowca::idNiezgodnyTypSurowca(0x01);
+	const STyp::Identyfikator BladKonfiguracjiEkranu::idBladKonfiguracjiEkranu(0x2);
+	const STyp::Identyfikator BladKonfiguracjiAplikacji::idBladKonfiguracjiAplikacji(0x1);
+
+	const STyp::Identyfikator	NiezgodnyTypSurowca::idNiezgodnyTypSurowca(0x10);
 	const STyp::Tekst		NiezgodnyTypSurowca::tytulNiezgodnyTypSurowcaDomyslny = "Niezgodny Typ Surowca";
 	const STyp::Tekst		NiezgodnyTypSurowca::trescNiezgodnyTypSurowcaDomyslny = "Brak zgodnosci typow uzytych surowcow.";
 
-	const STyp::Identyfikator	NieznalezionoObiektu::idNieznalezionoObiektu(0x09);
+	const STyp::Identyfikator	NieznalezionoObiektu::idNieznalezionoObiektu(0x19);
 	const STyp::Tekst		NieznalezionoObiektu::tytulNieznalezionoObiektu = "Nie znaleziono obiektu.";
 	const STyp::Tekst		NieznalezionoObiektu::trescNieznalezionoObiektu = "Obiekt o podanych kluczu nie zosta³ znaleziony.";
 
-	const STyp::Identyfikator	NiepoprawneParametryFunkcji::idNiepoprawneParametryFunkcji(0x0A);
+	const STyp::Identyfikator	NiepoprawneParametryFunkcji::idNiepoprawneParametryFunkcji(0x1A);
 	const STyp::Tekst		NiepoprawneParametryFunkcji::tytulNiepoprawneParametryFunkcji = "Niepoprawne parametry funkcji.";
 	const STyp::Tekst		NiepoprawneParametryFunkcji::trescNiepoprawneParametryFunkcji = "Do funkcji zosta³y przekazane niepoprawne parametry.";
 
-	const STyp::Identyfikator	NiepowodzenieTranzakcji::domyslnyNiepowodzenieTranzakcjiID(0x0C);
+	const STyp::Identyfikator	NiepowodzenieTranzakcji::domyslnyNiepowodzenieTranzakcjiID(0x1C);
 	const STyp::Tekst		NiepowodzenieTranzakcji::domyslnyNiepowodzenieTranzakcjiTytul = "Niepowodzenie wykonania tranzakcji";
 	const STyp::Tekst		NiepowodzenieTranzakcji::domyslnyNiepowodzenieTranzakcjiTresc = "Nie uda³o siê zachowaæ tanzakcyjnoœci wykonywanych operacji. Operacja powoduj¹ca wyj¹tek: ";
 
