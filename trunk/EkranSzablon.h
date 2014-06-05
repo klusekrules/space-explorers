ca97db1e56bcdf81c3378b8593a527a780b03554
danielwojdak@gmail.com <danielwojdak@gmail.com@7d2b72b5-99d5-17bd-2a6a-8f8c28692ed1>
2014-06-05 13:24:11 +0000

    [Issue-76] Dodanie dokumentacji do klas ekranów aplikacji. Usunięcie niepotrzebnych plików.

--- trunk/EkranSzablon.h
+++ trunk/EkranSzablon.h
@@ -1,38 +1,116 @@
 #pragma once
 #include "stdafx.h"
 #include "Stan.h"
+
 namespace SpEx{
+	/**
+	* \brief Klasa szablonowa dla nowych ekranow.
+	*
+	* Klasa bazowa dla klas ekranow. Najprostrza wersja klasy ekranu pozwalaj¹ca na wczytywnie danych z pliku. 
+	* Dostarcza uniwersalny interfejs u¿ywany w aplikacji.
+	* \author Daniel Wojdak
+	* \version 1
+	* \date 05-06-2014
+	*/
 	class EkranSzablon :
 		public sf::Drawable,
 		public sf::Transformable
 	{
 	public:
+		/**
+		* \brief Konstruktor.
+		* 
+		* Konstruktor buduj¹cy obiekt na podstawie wez³a z konfiguracj¹.
+		* \param[in] wezel - Weze³ konfiguracyjny.
+		* \throw WyjatekParser
+		*/
 		EkranSzablon(XmlBO::ElementWezla wezel);
 
-		virtual ~EkranSzablon(void) = default;
+		/**
+		* \brief Destruktor.
+		*/
+		virtual ~EkranSzablon() = default;
 
+		/**
+		* \brief Metoda przeliczaj¹ca parametry ekranu wzglêdem aktualnego stanu.
+		*
+		* Metoda przelicza prametry ekranu w zale¿noœci od stanu jaki zosta³ przekazany jako parametr.
+		* \param[in/out] stan - Aktualnie przetwarzany stan aplikacji.
+		*/
 		virtual void uaktualnij(Stan& stan);
 
+		/**
+		* \brief Metoda odbieraj¹ca zdarzenie.
+		*
+		* Metoda odbiera zdarzenie i przypisuje je do odpowiedniego okna nale¿¹cego do ekranu.
+		* \param[in/out] stan - Aktualnie przetwarzany stan aplikacji.
+		* \param[in] zdarzenie - Wykryte zdarzenie.
+		*/
 		virtual void odbierz(Stan& stan, const sf::Event& zdarzenie);
 
-		virtual void podlacz(sf::RenderWindow&);
+		/**
+		* \brief Metoda pod³¹czaj¹ca do okna.
+		*
+		* Metoda pod³¹cza ekran do okna przekazanego jako parametr.
+		* \param[in] okno - Okno do którego zostaje pod³¹czony ekran.
+		*/
+		virtual void podlacz(sf::RenderWindow& okno);
 
-		virtual void odlacz(sf::RenderWindow&);
+		/**
+		* \brief Metoda od³¹czaj¹ca do okna.
+		*
+		* Metoda od³¹cza ekran od okna przekazanego jako parametr.
+		* \param[in] okno - Okno do którego zostaje pod³¹czony ekran.
+		*/
+		virtual void odlacz(sf::RenderWindow& okno);
 
+		/**
+		* \brief Metoda pobieraj¹ca identyfikator ekranu.
+		*
+		* Metoda zwraca aktualny identyfikator ekranu.
+		* \return Aktualny identyfikator ekranu.
+		*/
 		const STyp::Identyfikator& pobierzId() const;
 
+		/**
+		* \brief Metoda pobieraj¹ca uchwyt do interfejsu GUI.
+		* 
+		* Metoda zwraca interfejs GUI na którym budowane s¹ okna danego ekranu.
+		* \return Referencja do interfejsu GUI.
+		*/
 		tgui::Gui& pobierzGUI();
 
+
+		/**
+		* \brief Metoda czyszcz¹ca ekran.
+		*
+		* Metoda usuwa wszystkie okna znajduj¹ce siê na danym ekranie.
+		*/
 		virtual void clear();
 
 	protected:
 
+		/**
+		* \brief Metoda konfiguruj¹ca kontrolkê.
+		*
+		* Metoda Konfiguruje wczytan¹ przez ekran kontrolkê na podstawie wez³a konfiguracyjnego.
+		* \param[in] wezel - Weze³ konfiguracyjny.
+		* \param[in] kontrolka - WskaŸnik do kontrolki, która ma zostaæ skonfigurowana.
+		* \throw WyjatekParser, BladKonfiguracjiEkranu
+		*/
 		bool wczytajDaneKontrolki(XmlBO::ElementWezla wezel, tgui::Widget::Ptr kontrolka);
 
+		/**
+		* \brief Metoda rysuj¹ca kontrolki.
+		*
+		* Metoda rysuj¹ca kontrolki na ekranie.
+		* \param[in] target - unused.
+		* \param[in] states - unused.
+		*/
 		void draw(sf::RenderTarget& target, sf::RenderStates states) const;
 
-		STyp::Identyfikator id_;
-		mutable tgui::Gui interfejs_;
+		STyp::Identyfikator id_; /// Identyfikator ekranu.
+		mutable tgui::Gui interfejs_; /// Interfejs GUI na którym tworzone s¹ kontrolki.
 	};
 };
 
