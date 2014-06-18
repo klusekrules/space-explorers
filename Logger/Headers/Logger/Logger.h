14a3d4ece3a89a37100bb0b5e77cef58e39d81a8
danielwojdak@gmail.com <danielwojdak@gmail.com@7d2b72b5-99d5-17bd-2a6a-8f8c28692ed1>
2014-06-18 11:16:26 +0000

    [Issue-76] Poprawienie komentarzy projektu Logger.

--- Logger/Headers/Logger/Logger.h
+++ Logger/Headers/Logger/Logger.h
@@ -5,14 +5,14 @@
 #include "LoggerSymbol.h"
 #include "LoggerInterface.h"
 
-/*LOGGER_EXPIMP_TEMPLATE template class LOGGER_API std::deque<bool>;
-LOGGER_EXPIMP_TEMPLATE template class LOGGER_API std::stack<bool>;*/
-
 namespace SLog{
 	/**
 	* \brief Klasa pomocnicza w tworzeniu napisu z innej klasy.
 	*
 	* Klasa s³u¿y do sklejania atrybutów i klas bazowych. U³atwia tworzenie napisów klas dziedzicz¹cych po LoggerInterface.
+	* \author Daniel Wojdak
+	* \version 2
+	* \date 18-06-2014
 	*/
 	class LOGGER_API Logger :
 		virtual public LoggerInterface
@@ -27,6 +27,9 @@ namespace SLog{
 		/**
 		* Konstruktor klasy Logger. Tworzy obiekt z nadan¹ nazw¹ klasy g³ównej.
 		* \param[in] nazwaKlasy - Nazwa klasy glównej dla której jest generowany napis.
+		* \author Daniel Wojdak
+		* \version 2
+		* \date 18-06-2014
 		*/
 		explicit Logger(const std::string& nazwaKlasy);
 
@@ -39,6 +42,9 @@ namespace SLog{
 		* Metoda statyczna tworz¹ca napis z obiektu przekazanego jako argument.
 		* \param[in] obiekt - Obiekt przekazany do utworzenia napisu.
 		* \return Napis z przekazanego obiektu.
+		* \author Daniel Wojdak
+		* \version 2
+		* \date 18-06-2014
 		*/
 		template< class T >
 		static std::string tworzPole(const T& obiekt){
@@ -52,6 +58,9 @@ namespace SLog{
 		* \param[in] nazwa - Nazwa pola.
 		* \param[in] obiekt - Obiekt przekazany do utworzenia napisu.
 		* \return Napis z przekazanego obiektu.
+		* \author Daniel Wojdak
+		* \version 2
+		* \date 18-06-2014
 		*/
 		template< class T >
 		static std::string tworzPole(const std::string& nazwa, const T& obiekt){
@@ -66,6 +75,9 @@ namespace SLog{
 		* \param[in] obiektPierwszy - Obiekt przekazany do utworzenia napisu.
 		* \param[in] obiektDrugi - Obiekt przekazany do utworzenia napisu.
 		* \return Utworzony napis.
+		* \author Daniel Wojdak
+		* \version 2
+		* \date 18-06-2014
 		*/
 		static std::string tworzPole(const std::string& nazwa, const LoggerInterface& obiektPierwszy, const LoggerInterface& obiektDrugi);
 
@@ -74,6 +86,9 @@ namespace SLog{
 		* \param[in] nazwa - Nazwa klasy.
 		* \param[in] opisKlasy - Opis zawartoœci klasy.
 		* \return Utworzony napis.
+		* \author Daniel Wojdak
+		* \version 2
+		* \date 18-06-2014
 		*/
 		static std::string tworzKlase(const std::string& nazwa, const std::string& opisKlasy);
 
@@ -81,6 +96,9 @@ namespace SLog{
 		* Metoda dodaj¹ca pole o podanej nazwie i zawartoœci.
 		* \param[in] nazwa - Nazwa pola.
 		* \param[in] obiekt - Zawartoœæ pola.
+		* \author Daniel Wojdak
+		* \version 2
+		* \date 18-06-2014
 		*/
 		void dodajPole(const std::string& nazwa, const LoggerInterface& obiekt);
 
@@ -88,6 +106,9 @@ namespace SLog{
 		* Metoda dodaj¹ca pole o podanej nazwie i zawartoœci.
 		* \param[in] nazwa - Nazwa pola.
 		* \param[in] opisPola - Zawartoœæ pola.
+		* \author Daniel Wojdak
+		* \version 2
+		* \date 18-06-2014
 		*/
 		void dodajPole(const std::string& nazwa, const std::string& opisPola);
 
@@ -96,6 +117,9 @@ namespace SLog{
 		* \param[in] nazwa - Nazwa pola.
 		* \param[in] obiektPierwszy - Zawartoœæ pierwszej czêœci pola.
 		* \param[in] obiektDrugi - Zawartoœæ drugiej czêœci pola.
+		* \author Daniel Wojdak
+		* \version 2
+		* \date 18-06-2014
 		*/
 		void dodajPole(const std::string& nazwa, const LoggerInterface& obiektPierwszy, const LoggerInterface& obiektDrugi);
 
@@ -104,29 +128,44 @@ namespace SLog{
 		* Metoda dodaj¹ca pole o podanej nazwie i zawartoœci.
 		* \param[in] nazwa - Nazwa pola.
 		* \param[in] obiekt - Zawartoœæ pola.
+		* \author Daniel Wojdak
+		* \version 2
+		* \date 18-06-2014
 		*/
 		void dodajPole(const std::string& nazwa, const std::shared_ptr<LoggerInterface> obiekt);
 
 		/**
 		* Metoda dodaj¹ca klase.
 		* \param[in] opisKlasy - Opis klasy.
+		* \author Daniel Wojdak
+		* \version 2
+		* \date 18-06-2014
 		*/
 		void dodajKlase(const std::string& opisKlasy);
 
 		/**
 		* Metoda s³u¿aca do zbudowania podklasy. Rozpoczyna dodawanie podklasy. Metody mog¹ byæ zag³ebiane jedna w drug¹.
 		* \param[in] nazwa - Nazwa podklasy.
+		* \author Daniel Wojdak
+		* \version 2
+		* \date 18-06-2014
 		*/
 		void rozpocznijPodKlase(const std::string& nazwa);
 
 		/**
 		* Metoda s³u¿aca do konczenia podklasy. Koñczy ona ostatnio otwart¹ podklasê.
+		* \author Daniel Wojdak
+		* \version 2
+		* \date 18-06-2014
 		*/
 		void zakonczPodKlase();
 
 		/**
 		* Funkcja s³u¿¹ca do tworzenia napisów z opisem klasy.
 		* \return Napis zawieraj¹cy opis klasy.
+		* \author Daniel Wojdak
+		* \version 2
+		* \date 18-06-2014
 		*/
 		std::string napis() const override;
 
@@ -134,11 +173,14 @@ namespace SLog{
 
 		/**
 		* Metoda sprawdzaj¹ca czy pole jest pierwsze. Je¿eli nie to dopisuje przecinek to ci¹gu znaków.
+		* \author Daniel Wojdak
+		* \version 2
+		* \date 18-06-2014
 		*/
 		void testPierwszegoPola();
 
 		std::stringstream napis_; /// Strumieñ zawieraj¹cy tworzony napis.
-		std::stack<bool> stos_ = std::move(std::stack<bool>({ true })); /// Stos zawieraj?cy informacje, kt?re pole jest pierwsze.
+		std::stack<bool> stos_ = std::move(std::stack<bool>({ true })); /// Stos zawieraj¹cy informacje, które pole jest pierwsze.
 
 	};
 }
--- Logger/Headers/Logger/LoggerNazwaKlasy.h
+++ Logger/Headers/Logger/LoggerNazwaKlasy.h
@@ -6,6 +6,9 @@ namespace SLog
 {
 	/**
 	* Pobieranie nazwy klasy w stringu.
+	* \author Daniel Wojdak
+	* \version 1
+	* \date 18-06-2014
 	*/
 #define NAZWAKLASY(C) SLog::LoggerNazwaKlasy::napis<C>()
 #define NAZWAPOLA(C) #C
@@ -13,12 +16,18 @@ namespace SLog
 	/**
 	* Klasa bazowa u¿ywana do stworzenia jednolitego interfajsu
 	* dla klasy generuj¹cej logi z aplikacji.
+	* \author Daniel Wojdak
+	* \version 1
+	* \date 18-06-2014
 	*/
 	class LOGGER_API LoggerNazwaKlasy {
 	public:
 		/**
 		* Funkjca pobieraj¹ca nazwê klasy przekazanej jako argument szablonu.
 		* \return Nazwa klasy przekazanej przez parametr szablonu.
+		* \author Daniel Wojdak
+		* \version 1
+		* \date 18-06-2014
 		*/
 		template< class C >
 		static std::string napis() {
