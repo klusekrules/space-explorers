0d5c5965ae527d3634338705f2199496b1f8205c
danielwojdak@gmail.com <danielwojdak@gmail.com@7d2b72b5-99d5-17bd-2a6a-8f8c28692ed1>
2014-06-18 11:16:26 +0000

    [Issue-76] Poprawienie komentarzy projektu Logger.

--- Headers/Logger/LoggerInterface.h
+++ Headers/Logger/LoggerInterface.h
@@ -7,12 +7,18 @@ namespace SLog{
 	/**
 	* Klasa bazowa u¿ywana do stworzenia jednolitego interfajsu
 	* dla klasy generuj¹cej logi z aplikacji.
+	* \author Daniel Wojdak
+	* \version 1
+	* \date 18-06-2014
 	*/
 	class LOGGER_API LoggerInterface {
 	public:
 		/**
 		* Funkcja s³u¿¹ca jako podstawa do tworzenia napisów z opisem klasy.
 		* \return Napis zawieraj¹cy opis klasy.
+		* \author Daniel Wojdak
+		* \version 1
+		* \date 18-06-2014
 		*/
 		virtual std::string napis() const abstract;
 	};
