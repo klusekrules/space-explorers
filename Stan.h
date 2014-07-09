958fa7a155b6c98f719e5442a0b64692575eea2a
danielwojdak@gmail.com <danielwojdak@gmail.com@7d2b72b5-99d5-17bd-2a6a-8f8c28692ed1>
2014-07-09 13:13:35 +0000

    [Issue-76] Dodanie dokumentacji dla klas Maszyny stanów.

--- Stan.h
+++ Stan.h
@@ -4,28 +4,124 @@
 #include <chrono>
 
 namespace SpEx{
+
+	/**
+	* \brief Klasa stanu
+	*
+	* Klasa zawiera informacje o stanie.
+	* \author Daniel Wojdak
+	* \version 1
+	* \date 09-07-2014
+	*/
 	class Stan
 	{
 	public:
-		typedef std::chrono::duration<long double, std::milli> KrokCzasu;
+		typedef std::chrono::duration<long double, std::milli> KrokCzasu; /// Typ czasu.
+		
+		/**
+		* \brief Konstruktor.
+		*
+		* Konstruktor.
+		* \param[in] info - Obiekt opisuj¹cy.
+		* \author Daniel Wojdak
+		* \version 1
+		* \date 09-07-2014
+		*/
+		Stan(std::shared_ptr<StanInfo> info);
 
-		Stan(std::shared_ptr<StanInfo>);
-		Stan(const Stan&,std::shared_ptr<StanInfo>);
+		/**
+		* \brief Konstruktor kopiuj¹cy.
+		*
+		* Konstruktor kopuj¹cy ze zmian¹ obiektu opisowego.
+		* \param[in] obiekt - Obiekt Ÿród³owy.
+		* \param[in] info - Obiekt opisuj¹cy.
+		* \author Daniel Wojdak
+		* \version 1
+		* \date 09-07-2014
+		*/
+		Stan(const Stan& obiekt, std::shared_ptr<StanInfo> info);
+
+		/**
+		* \brief Domyœlny destruktor.
+		*
+		* Domyœlny destruktor.
+		*/
 		~Stan() = default;
 
+		/**
+		* \brief Metoda wejœcia.
+		*
+		* Metoda wykonywana w momencie wejœcia do stanu.
+		* \return Zwracana jest wartoœæ true, je¿eli wykonanie sie powiedzie. Zwracana jest wartoœæ false w przypadku wyst¹pienia b³êdu.
+		* \author Daniel Wojdak
+		* \version 1
+		* \date 09-07-2014
+		*/
 		bool akcjaWejscia();
+
+		/**
+		* \brief Metoda wewnêtrzna.
+		*
+		* Metoda wykonywana w momencie ponownego wejœcia do tego samego stanu.
+		* \return Zwracana jest wartoœæ true, je¿eli wykonanie sie powiedzie. Zwracana jest wartoœæ false w przypadku wyst¹pienia b³êdu.
+		* \author Daniel Wojdak
+		* \version 1
+		* \date 09-07-2014
+		*/
 		bool akcjaWewnetrzna();
+
+		/**
+		* \brief Metoda wyjœcia.
+		*
+		* Metoda wykonywana w momencie wyjœcia ze stanu.
+		* \return Zwracana jest wartoœæ true, je¿eli wykonanie sie powiedzie. Zwracana jest wartoœæ false w przypadku wyst¹pienia b³êdu.
+		* \author Daniel Wojdak
+		* \version 1
+		* \date 09-07-2014
+		*/
 		bool akcjaWyjscia();
 
-		STyp::Identyfikator id_;
-		int numer_;
-		KrokCzasu dt_;
-		std::vector< STyp::Identyfikator > listaEkranow_;
-		bool operator==(const Stan&)const;
-		bool operator==(const Zdarzenie &)const;
+		STyp::Identyfikator id_; /// Identyfikator stanu.
+		int numer_; /// Numer dodatkowy.
+		KrokCzasu dt_; /// Krok czasu. U¿ywany w w¹tku graficznym.
+		std::vector< STyp::Identyfikator > listaEkranow_; /// Lista ekranów. U¿ywana w w¹tku graficznym.
+
+		/**
+		* \brief Operator porównania.
+		*
+		* Operator porównania.
+		* \param[in] stan - Stan do porównania.
+		* \return Zwracana jest wartoœæ true, je¿eli obiektu s¹ równowa¿ne. Zwracana jest wartoœæ false w przeciwnym wypadku.
+		* \author Daniel Wojdak
+		* \version 1
+		* \date 09-07-2014
+		*/
+		bool operator==(const Stan& stan)const;
+
+		/**
+		* \brief Operator porównania.
+		*
+		* Operator porównania.
+		* \param[in] zdarzenie - Zdarzenie do porównania.
+		* \return Zwracana jest wartoœæ true, je¿eli obiektu s¹ równowa¿ne. Zwracana jest wartoœæ false w przeciwnym wypadku.
+		* \author Daniel Wojdak
+		* \version 1
+		* \date 09-07-2014
+		*/
+		bool operator==(const Zdarzenie & zdarzenie)const;
+
+		/**
+		* \brief Metoda pobieraj¹ca opis stanu.
+		*
+		* Metoda pobieraj¹ca obiekt opisuj¹cy stan.
+		* \return Zwracany jest wskaŸnik na aktualny obiekt opisuj¹cy stan.
+		* \author Daniel Wojdak
+		* \version 1
+		* \date 09-07-2014
+		*/
 		std::shared_ptr<StanInfo> opisStanu() const;
 	private:
-		std::shared_ptr<StanInfo> info_;
+		std::shared_ptr<StanInfo> info_; /// Obiekt opisuj¹cy stan.
 	};
 };
 
--- TypyEksportowne.h
+++ TypyEksportowne.h
@@ -1,14 +1,28 @@
 extern "C"{ 
-
+	/**
+	* \brief Struktura opisuj¹ca stan
+	*
+	* Struktura przechowuje informacje o stanie aplikacji. Struktura przeznaczona do u¿ywania w skryptach.
+	* \author Daniel Wojdak
+	* \version 1
+	* \date 09-07-2014
+	*/
 	struct __declspec(dllexport) Stan_t{
-		int idStanu_;
-		int numer_;
-		//long double dt_; //Milisekundy
+		int idStanu_; /// Numer stanu.
+		int numer_; /// Numer dodatkowy.
 	};
 
+	/**
+	* \brief Struktura zdarzenia
+	*
+	* Struktura przechowuje informacje o zdarzeniu. Struktura przeznaczona do u¿ywania w skryptach.
+	* \author Daniel Wojdak
+	* \version 1
+	* \date 09-07-2014
+	*/
 	struct __declspec(dllexport) Zdarzenie_t{
-		int idStanu_;
-		int numer_;
-		int idZdarzenia_;
+		int idStanu_; /// Numer stanu.
+		int numer_; /// Numer dodatkowy.
+		int idZdarzenia_; /// Numer zdarzenia.
 	};
 };
