97cfd422d87d58d285fd519b9b28d63a407fc4eb
danielwojdak@gmail.com <danielwojdak@gmail.com@7d2b72b5-99d5-17bd-2a6a-8f8c28692ed1>
2014-09-16 13:24:12 +0000

    Dodanie testu surowca, dodanie makra testującego wystąpienie wyjątku.

---
+++ trunk/SurowceTest.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "TestyJednostkowe.h"
+#include "Aplikacja.h"
+
+class SurowceTest:
+	public PaczkaTestow
+{
+	TWORZ_PACZKE_TESTOW(SurowceTest);
+		DODAJ_TEST(TworzenieSurowcaTest);
+		DODAJ_TEST(DodawanieSurowcaTest);
+	TWORZ_PACZKE_TESTOW_END();
+public:
+	SurowceTest() = default;
+	void TworzenieSurowcaTest();
+	void DodawanieSurowcaTest();
+	~SurowceTest();
+private:
+	SpEx::Surowce* krysztal_ = nullptr;
+	SpEx::Surowce* krysztal2_ = nullptr;
+	SpEx::Surowce* metal_ = nullptr;
+};
+
