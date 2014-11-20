bcf6e164c8017fff93507ad923db1be6a1bf6cc1
danielwojdak@gmail.com <danielwojdak@gmail.com@7d2b72b5-99d5-17bd-2a6a-8f8c28692ed1>
2014-11-20 13:04:18 +0000

    Poprawa streści wyświetlanych w logach.

--- Headers/Logger/LoggerNazwaKlasy.h
+++ Headers/Logger/LoggerNazwaKlasy.h
@@ -10,16 +10,16 @@ namespace SLog
 	* \version 1
 	* \date 18-06-2014
 	*/
-#define NAZWAKLASY(C) SLog::LoggerNazwaKlasy::napis<C>()
-#define NAZWAKLASY2(C) SLog::LoggerNazwaKlasy::napis(C)
+#define NAZWAKLASY(C) typeid(C).name()
+#define NAZWAKLASY2(C) typeid(C).name()
 #define NAZWAPOLA(C) #C
 
 	/**
 	* Klasa bazowa u¿ywana do stworzenia jednolitego interfajsu
 	* dla klasy generuj¹cej logi z aplikacji.
 	* \author Daniel Wojdak
-	* \version 2
-	* \date 17-11-2014
+	* \version 3
+	* \date 20-11-2014
 	*/
 	class LOGGER_API LoggerNazwaKlasy {
 	public:
@@ -27,29 +27,27 @@ namespace SLog
 		* Funkjca pobieraj¹ca nazwê klasy przekazanej jako argument szablonu.
 		* \return Nazwa klasy przekazanej przez parametr szablonu.
 		* \author Daniel Wojdak
-		* \version 2
-		* \date 17-11-2014
+		* \version 3
+		* \date 20-11-2014
 		*/
 		template< class C >
 		static std::string napis() {
 			std::string name(typeid(C).name());
-			if (name.size() > 6){
-				return std::move(name.substr(6));
-			}
-			else{
-				return std::move(name);
-			}
+			return std::move(name);
 		}
 
+		/**
+		* Funkjca pobieraj¹ca nazwê klasy przekazanej jako argument szablonu.
+		* \param[in] e - element którego typ zostanie zwrócony.
+		* \return Nazwa klasy przekazanej przez parametr szablonu.
+		* \author Daniel Wojdak
+		* \version 1
+		* \date 20-11-2014
+		*/
 		template< class C >
 		static std::string napis( const C & e ) {
 			std::string name(typeid(e).name());
-			if (name.size() > 6){
-				return std::move(name.substr(6));
-			}
-			else{
-				return std::move(name);
-			}
+			return std::move(name);
 		}
 	};
 }
