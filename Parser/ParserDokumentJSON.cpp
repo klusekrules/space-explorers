eb1ab2e833f5ffaaa1799883a1618c1dcb1360af
danielwojdak@gmail.com <danielwojdak@gmail.com@7d2b72b5-99d5-17bd-2a6a-8f8c28692ed1>
2015-01-09 14:42:25 +0000

    Opakowanie parsera json w klasy parsera używane przez aplikacje.

---
+++ Parser/ParserDokumentJSON.cpp
@@ -0,0 +1,63 @@
+#include "ParserDokumentJSON.h"
+#include "ParserElementJSON.h"
+
+namespace SPar{
+	std::shared_ptr<ParserElement> ParserDokumentJSON::pobierzElement(const char* nazwa){
+		if (root_.isObject()){
+			for (auto iter = root_.begin(); iter != root_.end(); ++iter){
+				if (strcmp(iter.memberName(), nazwa) == 0){
+					return std::make_shared<ParserElementJSON>(iter, root_.end());
+				}
+			}
+		}
+		return nullptr;
+	}
+
+	std::shared_ptr<ParserElement> ParserDokumentJSON::tworzElement(const char* nazwa){
+		if (root_.isObject()){
+			root_[nazwa] = Json::objectValue;
+			return pobierzElement(nazwa);
+		}
+		return nullptr;
+	}
+
+	bool ParserDokumentJSON::usunElement(std::shared_ptr<ParserElement> element){
+		if (element)
+			return !root_.removeMember(element->pobierzNazwe()).isNull();
+		return false;
+	}
+
+	bool ParserDokumentJSON::odczytaj(const char* nazwa){
+		if (!nazwa)
+			return false;
+		nazwa_ = nazwa;
+		return; // TODO:: Wczytywanie pliku
+	}
+
+	bool ParserDokumentJSON::zapisz(const char* nazwa){
+		if (nazwa){
+			nazwa_ = nazwa;
+			return;// TODO:: Zapisywanie pliku
+		}
+		else{
+			if (nazwa_.empty())
+				return false;
+			return; // TODO:: Zapisywanie pliku
+		}
+	}
+
+	ParserDokumentJSON::operator bool()const{
+		return !root_.isNull();
+	}
+
+	std::string ParserDokumentJSON::error()const{
+		Json::FastWriter printer;
+		if (!root_.isNull()){
+			return printer.write(root_);
+		}
+		else{
+			return std::string("nullptr");
+		}
+	}
+
+}
