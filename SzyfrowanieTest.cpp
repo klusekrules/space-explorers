d272c1f734652e25f6ee4f7fc4dab2a3a0928561
danielwojdak@gmail.com <danielwojdak@gmail.com@7d2b72b5-99d5-17bd-2a6a-8f8c28692ed1>
2015-01-26 13:25:58 +0000

    [Issue-120][Issue-121] Dodanie testu kompresji z szyfrowaniem

--- SzyfrowanieTest.cpp
+++ SzyfrowanieTest.cpp
@@ -8,10 +8,10 @@ void SzyfrowanieTest::szyfrowanieTest()
 	VMPC_MAC szyfr;
 	SHA3 klucz("haslo");
 	SHA3 wektor("data");
-	std::string wiadomoscOrg("Moja wiadomosc");
+	std::string wiadomoscOryginalna("Moja wiadomosc");
 	std::string wiadomosc;
 
-	wiadomosc = wiadomoscOrg;
+	wiadomosc = wiadomoscOryginalna;
 
 	szyfr.InitKey(klucz.pobierzKontener(),wektor.pobierzKontener());
 	szyfr.EncryptMAC(wiadomosc);
@@ -24,29 +24,69 @@ void SzyfrowanieTest::szyfrowanieTest()
 	auto& macDe = deSzyfr.OutputMAC();
 
 	UNIT_TEST_ASSERT_EQUAL(mac, macDe);
-	UNIT_TEST_ASSERT_EQUAL(wiadomosc, wiadomoscOrg);
+	UNIT_TEST_ASSERT_EQUAL(wiadomosc, wiadomoscOryginalna);
 
 }
 
 void SzyfrowanieTest::kompresjaTest(){
-	std::string wiadomoscOrg("Moja wiadomosc , Moja wiadomosc, Moja wiadomosc, Moja wiadomosc, Moja wiadomosc, Moja wiadomosc");
+	std::string wiadomoscOryginalna("Moja wiadomosc , Moja wiadomosc, Moja wiadomosc, Moja wiadomosc, Moja wiadomosc, Moja wiadomosc");
 	
-	auto bound = compressBound(wiadomoscOrg.size());
+	auto bound = compressBound(wiadomoscOryginalna.size());
 	uLongf after = bound;
 	std::vector<Bytef> out;
 	out.resize(after,0);
-	compress((Bytef*)out.data(),&after , (const Bytef*)wiadomoscOrg.data(), wiadomoscOrg.size());
+	compress((Bytef*)out.data(), &after, (const Bytef*)wiadomoscOryginalna.data(), wiadomoscOryginalna.size());
 	
 
 	SLog::Log::pobierzInstancje().loguj(SLog::Log::Info, std::to_string(bound));
 
-	uLongf unSize = wiadomoscOrg.size();
+	uLongf unSize = wiadomoscOryginalna.size();
 	std::string uncompres;
 	uncompres.resize(unSize, 0);
 	uncompress((Bytef*)uncompres.data(), &unSize, out.data(), after);
 
-	UNIT_TEST_ASSERT_EQUAL(uncompres, wiadomoscOrg);
+	UNIT_TEST_ASSERT_EQUAL(uncompres, wiadomoscOryginalna);
 }
 
+void SzyfrowanieTest::kompresjaOrazSzyfrowanieTest(){
+	std::string wiadomoscOryginalna("Moja wiadomosc , Moja wiadomosc, Moja wiadomosc, Moja wiadomosc, Moja wiadomosc, Moja wiadomosc");
+	std::string wiadomoscOtrzymana;
+	SHA3 klucz("haslo");
+	SHA3 wektor("data");
+	VMPC_MAC szyfrowanie;
+	VMPC_MAC deszyfrowanie;
+
+	//Obliczanie maksymalnego rozmiaru bufora do przechowania skompresowanej wiadomoœci
+	auto bound = compressBound(wiadomoscOryginalna.size());
+	SLog::Log::pobierzInstancje().loguj(SLog::Log::Info, std::to_string(bound));
+
+	//Kompresja wiadomoœci
+	uLongf after = bound;
+	std::string out;
+	out.resize(after, 0);
+	compress((Bytef*)out.data(), &after, (const Bytef*)wiadomoscOryginalna.data(), wiadomoscOryginalna.size());
+	out.resize(after);
+
+	SLog::Log::pobierzInstancje().loguj(SLog::Log::Info, std::to_string(after));
+
+	//Szyfrowanie wiadomoœci
+	szyfrowanie.InitKey(klucz.pobierzKontener(), wektor.pobierzKontener());
+	szyfrowanie.EncryptMAC(out);
+	auto& sumaKontrolnaSzyfrowania = szyfrowanie.OutputMAC();
+
+	//Deszyfrowywanie wiadomoœci
+	deszyfrowanie.InitKey(klucz.pobierzKontener(), wektor.pobierzKontener());
+	deszyfrowanie.DecryptMAC(out);
+	auto& sumaKontrolnaDeszyfrowania = deszyfrowanie.OutputMAC();
+
+	UNIT_TEST_ASSERT_EQUAL(sumaKontrolnaSzyfrowania, sumaKontrolnaDeszyfrowania);
+	
+	//Dekompresja wiadomoœci
+	uLongf unSize = wiadomoscOryginalna.size();
+	wiadomoscOtrzymana.resize(unSize, 0);
+	uncompress((Bytef*)wiadomoscOtrzymana.data(), &unSize, (const Bytef*)out.data(), after);
+
+	UNIT_TEST_ASSERT_EQUAL(wiadomoscOtrzymana, wiadomoscOryginalna);
+}
 
 REJESTRUJ_PACZKE_TESTOW(SzyfrowanieTest);
