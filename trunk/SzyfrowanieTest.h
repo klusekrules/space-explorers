4652074647cc39d040db818c089aa5d5fcfccecf
danielwojdak@gmail.com <danielwojdak@gmail.com@7d2b72b5-99d5-17bd-2a6a-8f8c28692ed1>
2015-01-26 13:25:58 +0000

    [Issue-120][Issue-121] Dodanie testu kompresji z szyfrowaniem

--- trunk/SzyfrowanieTest.h
+++ trunk/SzyfrowanieTest.h
@@ -8,11 +8,13 @@ class SzyfrowanieTest :
 	TWORZ_PACZKE_TESTOW(SzyfrowanieTest);
 		DODAJ_TEST(szyfrowanieTest);
 		DODAJ_TEST(kompresjaTest);
+		DODAJ_TEST(kompresjaOrazSzyfrowanieTest);
 	TWORZ_PACZKE_TESTOW_END();
 public:
 	SzyfrowanieTest() = default;
 	void szyfrowanieTest();
 	void kompresjaTest();
+	void kompresjaOrazSzyfrowanieTest();
 	virtual ~SzyfrowanieTest() = default;
 };
 
