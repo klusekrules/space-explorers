ac4d2dc4eb92a1141fe63645b65735a9275c16be
danielwojdak@gmail.com <danielwojdak@gmail.com@7d2b72b5-99d5-17bd-2a6a-8f8c28692ed1>
2015-02-25 12:27:54 +0000

    [Issue-124] Wysyłanie potwierdzenia logowania do serwera.

--- PotwierdzLogowanieRPC.h
+++ PotwierdzLogowanieRPC.h
@@ -2,21 +2,21 @@
 #include "MetodaRPC.h"
 
 namespace SpEx{
-	class ZalogujRPC :
+	class PotwierdzLogowanieRPC :
 		virtual public SLog::LoggerInterface,
 		public MetodaRPC
 	{
 	public:
-		friend class std::_Ref_count_obj<ZalogujRPC>;
+		friend class std::_Ref_count_obj<PotwierdzLogowanieRPC>;
 		static bool RejestratorMetodyRPC(Fabrykator& fabryka, SLog::Log& logger);
 		static std::shared_ptr<MetodaRPC> TworzObiekt(const Json::Value &, Klient&);
 
-		virtual ~ZalogujRPC() = default;
+		virtual ~PotwierdzLogowanieRPC() = default;
 		std::string napis() const override;
 
 	private:
 
-		ZalogujRPC(Klient&);
+		PotwierdzLogowanieRPC(Klient&);
 		void obslugaZadania(const Json::Value &, Json::Value&) override;		
 		bool obslugaOdpowiedzi(const Json::Value &) override;
 		
